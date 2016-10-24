262a06922e014371e973581949d4e27411659089
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-24 18:21:19 -0200

    Update UploadedImage viewset

--- ovp_uploads/views.py
+++ ovp_uploads/views.py
@@ -15,7 +15,10 @@ class UploadedImageViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
   def create(self, request, *args, **kwargs):
     # Create mutable copy of request data
     data = copy.deepcopy(request.data)
-    data._mutable=True
+    try:
+      data._mutable=True
+    except AttributeError: #pragma: no cover
+      pass
 
     data.pop('user', None)
     upload_header = request.META.get('HTTP_X_UNAUTHENTICATED_UPLOAD', None)
@@ -31,14 +34,9 @@ class UploadedImageViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
       serializer = self.get_serializer(data=data)
 
       if serializer.is_valid():
-        self.pre_save(serializer.object)
-        self.object = serializer.save(force_insert=True)
-        self.post_save(self.object, created=True)
+        self.object = serializer.save()
         headers = self.get_success_headers(serializer.data)
 
-        if not data['user']:
-          serializer.data['warning'] = _('Files uploaded with X-Unauthenticated-Upload header are periodically removed. Please upload while authenticated.')
         return response.Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
       return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
     return response.Response(status=status.HTTP_401_UNAUTHORIZED)
