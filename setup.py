2628194b90d0017ec6214b47285606e5d39dc5f6
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-26 13:02:18 -0200

    Release 0.1.4

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='ovp-uploads',
-    version='0.1.3',
+    version='0.1.4',
     author=u'Atados',
     author_email='arroyo@atados.com.br',
     packages=find_packages(),
     url='https://github.com/OpenVolunteeringPlatform/django-ovp-uploads',
-    download_url = 'https://github.com/OpenVolunteeringPlatform/django-ovp-core/tarball/0.1.3',
+    download_url = 'https://github.com/OpenVolunteeringPlatform/django-ovp-core/tarball/0.1.4',
     license='AGPL',
     description='This module has core upload functionality.',
     long_description=open('README.rst', encoding='utf-8').read(),
