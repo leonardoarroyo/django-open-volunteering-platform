74cd27821eff7f92ed84acd49cfa2f150501ddb9
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-11-03 19:43:40 -0200

    Return null if no image file is associated with UploadedImage model

--- ovp_uploads/serializers.py
+++ ovp_uploads/serializers.py
@@ -2,6 +2,10 @@ from ovp_uploads.models import UploadedImage
 
 from rest_framework import serializers
 
+def build_absolute_uri(req, image):
+  return self.context['request'].build_absolute_uri(image.url) if image else None
+
+
 class UploadedImageSerializer(serializers.ModelSerializer):
   image_url = serializers.SerializerMethodField()
   image_small_url = serializers.SerializerMethodField()
@@ -15,13 +19,13 @@ class UploadedImageSerializer(serializers.ModelSerializer):
     extra_kwargs = {'image': {'write_only': True}}
 
   def get_image_url(self, obj):
-    return self.context['request'].build_absolute_uri(obj.image.url)
+    return build_absolute_uri(self.context['request'], obj.image)
 
   def get_image_small_url(self, obj):
-    return self.context['request'].build_absolute_uri(obj.image_small.url)
+    return build_absolute_uri(self.context['request'], obj.image_small)
 
   def get_image_medium_url(self, obj):
-    return self.context['request'].build_absolute_uri(obj.image_medium.url)
+    return build_absolute_uri(self.context['request'], obj.image_medium)
 
   def get_image_large_url(self, obj):
-    return self.context['request'].build_absolute_uri(obj.image_large.url)
+    return build_absolute_uri(self.context['request'], obj.image_large)
