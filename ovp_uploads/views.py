39e905dbe6b069b4c44d77bc4d2bc44f981c76be
Cidiomar Jr <cidiomar.jr@gmail.com>
2017-02-07 18:32:04 -0200

    fix deepcopy issue with UploadedImageViewSet#create

--- ovp_uploads/views.py
+++ ovp_uploads/views.py
@@ -13,30 +13,26 @@ class UploadedImageViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
   serializer_class = UploadedImageSerializer
 
   def create(self, request, *args, **kwargs):
-    # Create mutable copy of request data
-    data = copy.deepcopy(request.data)
-    try:
-      data._mutable=True
-    except AttributeError: #pragma: no cover
-      pass
+    req_data, req_user, upload_data = request.data, request.user, {}
 
-    data.pop('user', None)
+    upload_data['image'] = req_data.get('image') # note : raise bad request (400) on image missing?
+
     upload_header = request.META.get('HTTP_X_UNAUTHENTICATED_UPLOAD', None)
-    is_authenticated = request.user.is_authenticated()
+    is_authenticated = req_user.is_authenticated()
 
     if is_authenticated or upload_header:
       if upload_header:
-        data['user'] = None
+        upload_data['user'] = None
 
       if is_authenticated:
-        data['user'] = request.user.id
+        upload_data['user'] = request.user.id
 
-      serializer = self.get_serializer(data=data)
+      serializer = self.get_serializer(data=upload_data)
 
       if serializer.is_valid():
         self.object = serializer.save()
         headers = self.get_success_headers(serializer.data)
-
         return response.Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
       return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     return response.Response(status=status.HTTP_401_UNAUTHORIZED)
