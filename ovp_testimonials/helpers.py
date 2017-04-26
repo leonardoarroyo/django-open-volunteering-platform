526e5453526b8b42cd0ef54c2588b2a876b76a8e
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-04-26 14:43:46 -0300

    Remove django-haystack

--- ovp_testimonials/helpers.py
+++ ovp_testimonials/helpers.py
@@ -1,6 +1,4 @@
 from django.conf import settings
-from haystack import connection_router, connections
-from haystack.inputs import Raw
 
 def get_settings(string="OVP_TESTIMONIALS"):
   return getattr(settings, string, {})
