33fd6ce03e1371b2352b3e38fbe5ac4b387e8fa7
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-04-26 15:38:18 -0300

    Add meta fields to Testimonial model

--- ovp_testimonials/models.py
+++ ovp_testimonials/models.py
@@ -6,3 +6,11 @@ class Testimonial(models.Model):
   content = models.TextField(_('Company Relationship'), max_length=3000)
   rating = models.FloatField(_('Rating'), validators=[MinValueValidator(0), MaxValueValidator(10)])
   user = models.ForeignKey('ovp_users.User')
+
+  # Meta
+  published = models.BooleanField(_('Published'), default=False)
+  published_date = models.DateTimeField(_('Published date'), blank=True, null=True)
+  deleted = models.BooleanField(_('Deleted'), default=False)
+  deleted_date = models.DateTimeField(_('Deleted date'), blank=True, null=True)
+  created_date = models.DateTimeField(_('Created date'), auto_now_add=True)
+  modified_date = models.DateTimeField(_('Modified date'), auto_now=True)
