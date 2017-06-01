7f71497e2b2bf1e2f7a59549acfaa032be8dfb2a
Cidiomar Jr <cidiomar.jr@gmail.com>
2017-06-01 18:47:32 -0300

    impl admin panel

--- ovp_testimonials/admin.py
+++ ovp_testimonials/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django import forms
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from .models import Testimonial
+
+
+class TestimonialAdmin(admin.ModelAdmin):
+  fields = [
+    ('id', 'rating'),
+    ('user'),
+    ('published', 'created_date'),
+    'content'
+    ]
+
+  list_display = [
+    'id', 'user', 'rating', 'published', 'created_date'
+    ]
+
+  list_filter = [
+    'published', 'rating'
+    ]
+
+  list_editable = ['published']
+
+  search_fields = [
+    'user__name', 'content'
+    ]
+
+  readonly_fields = ['id', 'created_date']
+  raw_id_fields = []
+
+
+admin.site.register(Testimonial, TestimonialAdmin)
+admin.site._registry[Testimonial].display_on_main_menu = True
--- ovp_testimonials/models.py
+++ ovp_testimonials/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Testimonial(models.Model):
-  content = models.TextField(_('Company Relationship'), max_length=3000)
+  content = models.TextField(_('Contents'), max_length=3000)
   rating = models.FloatField(_('Rating'), validators=[MinValueValidator(0), MaxValueValidator(10)], null=True, blank=True)
   user = models.ForeignKey('ovp_users.User', null=True, blank=True)
 
