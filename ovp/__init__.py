8b1d8b6f0032ff49336b8f346b3b81b4f9194f17
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-07-17 17:51:36 -0300

    Add get_core_apps.

---
+++ ovp/__init__.py
@@ -0,0 +1,12 @@
+
+def get_core_apps(self):
+  """ Get core ovp apps list.
+
+  Returns:
+    list: A list with all ovp apps to be appended to INSTALLED_APPS on settings.py
+  """
+  CORE_APPS = [
+    "ovp.apps.core"
+  ]
+
+  return CORE_APPS
