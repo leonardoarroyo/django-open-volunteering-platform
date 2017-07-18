4d627c205265dca5e94b5fab91517fafe1a21541
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-07-18 19:04:34 -0300

    Move single-app files to deprecated and app modules to app root

--- ovp/apps/uploads/apps.py
+++ ovp/apps/uploads/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class UploadsConfig(AppConfig):
-    name = 'ovp.apps.uploads'
+    name = 'uploads'
--- ovp/apps/uploads/models.py
+++ ovp/apps/uploads/models.py
@@ -48,7 +48,7 @@ class UploadedImage(models.Model):
     return self.uuid
 
   class Meta:
-    app_label = 'ovp.apps.uploads'
+    app_label = 'uploads'
     verbose_name = _('uploaded image')
     verbose_name_plural = _('uploaded images')
 
