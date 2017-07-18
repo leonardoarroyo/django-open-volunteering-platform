911df8beddbed23b10e4040b5f3d41220d0c218a
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-07-18 14:02:40 -0300

    Add ovp.apps.uploads to core apps

--- ovp/__init__.py
+++ ovp/__init__.py
@@ -6,7 +6,8 @@ def get_core_apps():
     list: A list with all ovp apps to be appended to INSTALLED_APPS on settings.py
   """
   CORE_APPS = [
-    "ovp.apps.core"
+    "ovp.apps.core",
+    "ovp.apps.uploads"
   ]
 
   return CORE_APPS
