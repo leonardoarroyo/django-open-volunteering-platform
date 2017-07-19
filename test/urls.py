def5aae0d9be4248dfb732d43635e98f2ea89eea
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-07-18 21:58:47 -0300

    Add uploads to test runner urls.py

--- test/urls.py
+++ test/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url, include
 
 import ovp.apps.core.urls
+import ovp.apps.uploads.urls
 
 urlpatterns = [
+  # Core
   url(r'^', include(ovp.apps.core.urls)),
+
+  # Uploads
+  url(r'^', include(ovp.apps.uploads.urls)),
 ]
