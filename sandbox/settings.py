a62d91ab76f630211660baea0de83e36ea96c2d0
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-07-21 19:52:06 -0300

    Add apps to sandbox

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ] + get_core_apps()
+
+AUTH_USER_MODEL='users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- sandbox/urls.py
+++ sandbox/urls.py
@@ -1,21 +1,25 @@
-"""sandbox URL Configuration
+from django.conf.urls import url, include
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
-from django.contrib import admin
+import ovp.apps.core.urls
+import ovp.apps.uploads.urls
+import ovp.apps.users.urls
+import ovp.apps.projects.urls
+import ovp.apps.organizations.urls
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+  # Core
+  url(r'^', include(ovp.apps.core.urls)),
+
+  # Uploads
+  url(r'^', include(ovp.apps.uploads.urls)),
+
+  # Users
+  url(r'^', include(ovp.apps.users.urls)),
+
+  # Projects
+  url(r'^', include(ovp.apps.projects.urls)),
+
+  # Organizations
+  url(r'^', include(ovp.apps.organizations.urls)),
 ]
+
