81f1f9998bed8e12eabb7a7692d1f021cc358bc7
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-07-28 18:21:52 -0300

    Associate with default channel by default

--- ovp/apps/channels/models/channel.py
+++ ovp/apps/channels/models/channel.py
@@ -5,7 +5,17 @@ class Channel(models.Model):
 	name = models.CharField(_('Name'), max_length=100)
 
 class ChannelRelationship(models.Model):
-  channels = models.ManyToManyField(Channel)
+  channels = models.ManyToManyField(Channel, related_name="channels")
 
   class Meta:
     abstract = True
+
+  def save(self, *args, **kwargs):
+    creating = False
+    if not self.pk:
+      creating = True
+
+    super(ChannelRelationship, self).save(*args, **kwargs)
+
+    if creating:
+      self.channels.add(Channel.objects.get(name="default"))
--- ovp/apps/channels/tests/test_models.py
+++ ovp/apps/channels/tests/test_models.py
@@ -1,6 +1,20 @@
+from django.test import TestCase
 from ovp.apps.channels.models import Channel
+from ovp.apps.projects.models import Project
+from ovp.apps.users.models import User
 
-class ChannelTestCase():
+class ChannelTestCase(TestCase):
   def test_migrations_create_default_channel(self):
     self.assertTrue(Channel.objects.count() == 1)
     self.assertTrue(Channel.objects.first().name == "default")
+
+  def test_models_that_extend_channel_relationship(self):
+    """ Assert models that extend ChannelRelationship model automatically get associated with default channel """
+    user = User(email="test@user.com", password="test_password")
+    user.save()
+
+    project = Project(name="test", owner=user)
+    project.save()
+
+    self.assertTrue(project.channels.all().count() == 1)
+    self.assertTrue(project.channels.first().name == "default")
