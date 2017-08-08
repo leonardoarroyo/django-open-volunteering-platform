1a53b5ad10837b4222013b0a834808f7efdc604f
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-08-08 17:32:38 -0300

    Use Project model in MultiChannel models test instead of User model

--- ovp/apps/channels/tests/helpers/views.py
+++ ovp/apps/channels/tests/helpers/views.py
@@ -8,7 +8,15 @@ from ovp.apps.channels.viewsets.mixins import CreateModelWithChannelMixin
 from ovp.apps.users.models import User
 from ovp.apps.users.serializers import UserCreateSerializer
 
+from ovp.apps.projects.models.project import Project
+from ovp.apps.projects.serializers.project import ProjectCreateUpdateSerializer
+
 @ChannelViewSet
 class ChannelUserTestViewSet(CreateModelWithChannelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
   queryset = User.objects.all().order_by("pk")
   serializer_class = UserCreateSerializer
+
+@ChannelViewSet
+class ChannelProjectTestViewSet(CreateModelWithChannelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+  queryset = Project.objects.all().order_by("pk")
+  serializer_class = ProjectCreateUpdateSerializer
