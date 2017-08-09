52ba25dd2b69e86a99ce83111dd592df2b079332
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-08-09 18:42:13 -0300

    Add error messages to channels exceptions

--- ovp/apps/channels/exceptions.py
+++ ovp/apps/channels/exceptions.py
@@ -1,5 +1,7 @@
 class UnexpectedMultipleChannelsError(Exception):
-  pass
+  def __init__(self):
+    super(Exception, self).__init__("You have passed multiple channels down to a single channel resource.")
 
 class UnexpectedChannelAssociationError(Exception):
-  pass
+  def __init__(self):
+    super(Exception, self).__init__("You can't associate a channel directly to single channel models. Pass object_channels to .save() or objects.create() method instead.")
