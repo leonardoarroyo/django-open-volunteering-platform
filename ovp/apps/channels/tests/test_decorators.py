853c6af9b37d6c81996ae68ba59048147352233b
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-08-09 18:51:55 -0300

    Test channels decorators with projects instead of users

--- ovp/apps/channels/tests/test_decorators.py
+++ ovp/apps/channels/tests/test_decorators.py
@@ -3,9 +3,11 @@ from django.test.client import RequestFactory
 
 from ovp.apps.users.models import User
 
+from ovp.apps.projects.models.project import Project
+
 from ovp.apps.channels.models import Channel
+from ovp.apps.channels.tests.helpers.views import ChannelProjectTestViewSet
 from ovp.apps.channels.middlewares.channel import ChannelMiddleware
-from ovp.apps.channels.tests.helpers.views import ChannelUserTestViewSet
 
 
 class ChannelViewsetDecoratorTestCase(TestCase):
@@ -18,22 +20,25 @@ class ChannelViewsetDecoratorTestCase(TestCase):
     channel1 = Channel(name="Channel One", slug="channel1")
     channel1.save()
 
-    # Set up test users
-    user1 = User(email="test@default.com", password="abc")
-    user1.save()
+    # Set up test projects
+    user = User(email="test@default.com", password="abc")
+    user.save()
 
-    user2 = User(email="test@channel1.com", password="abc")
-    user2.save()
-    user2.channels.clear()
-    user2.channels.add(channel1)
+    project1 = Project.objects.create(name="test", owner=user, object_channels=["test-channel"])
+    project1.save()
 
-    user3 = User(email="test@default+channel1.com", password="abc")
-    user3.save()
-    user3.channels.add(channel1)
+    project2 = Project.objects.create(name="test", owner=user, object_channels=["test-channel"])
+    project2.save()
+    project2.channels.clear()
+    project2.channels.add(channel1)
+
+    project3 = Project.objects.create(name="test", owner=user, object_channels=["test-channel"])
+    project3.save()
+    project3.channels.add(channel1)
 
     # Set up test view
     self.factory = RequestFactory()
-    self.cm = ChannelMiddleware(ChannelUserTestViewSet.as_view({'get': 'list'})) # We also pass it through the middleware
+    self.cm = ChannelMiddleware(ChannelProjectTestViewSet.as_view({'get': 'list'})) # We also pass it through the middleware
 
   def test_channels_restriction(self):
     self.request = self.factory.get("/test/")
