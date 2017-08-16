1e0af6b0b200851a03d3ab1f8c0223ff2c724fd5
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-08-16 15:34:06 -0300

    Add headers parameter to users test helpers

--- ovp/apps/users/tests/helpers.py
+++ ovp/apps/users/tests/helpers.py
@@ -13,7 +13,7 @@ def create_user(email="validemail@gmail.com", password="validpassword", extra_da
   return client.post(reverse('user-list'), data, format="json", **headers)
 
 
-def create_user_with_profile(email="validemail@gmail.com", password="validpassword", profile={}):
+def create_user_with_profile(email="validemail@gmail.com", password="validpassword", profile={}, **headers):
   data = {
     'name': 'Valid Name',
     'email': email,
@@ -22,22 +22,22 @@ def create_user_with_profile(email="validemail@gmail.com", password="validpasswo
   }
 
   client = APIClient()
-  return client.post(reverse('user-list'), data, format="json")
+  return client.post(reverse('user-list'), data, format="json", **headers)
 
-def authenticate(email='test_can_login@test.com', password='validpassword'):
+def authenticate(email='test_can_login@test.com', password='validpassword', **headers):
   data = {
     'email': email,
     'password': password
   }
 
   client = APIClient()
-  return client.post('/api-token-auth/', data, format="json")
+  return client.post('/api-token-auth/', data, format="json", **headers)
 
 
-def create_token(email='test@recovery.token'):
+def create_token(email='test@recovery.token', **headers):
   data = {
     'email': email,
   }
 
   client = APIClient()
-  return client.post(reverse('recovery-token-list'), data, format="json")
+  return client.post(reverse('recovery-token-list'), data, format="json", **headers)
