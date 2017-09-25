7172b4bcc067b27d5feed67451deccc9b8ca357b
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-09-25 14:21:38 -0300

    Move cache logic out of search views to @cached decorator

--- ovp/apps/search/views.py
+++ ovp/apps/search/views.py
@@ -14,6 +14,7 @@ from ovp.apps.users.models.profile import get_profile_model, UserProfile
 
 from ovp.apps.search import helpers
 from ovp.apps.search import filters
+from ovp.apps.search.decorators import cached
 
 from ovp.apps.channels.cache import get_channel_setting
 
@@ -32,35 +33,33 @@ class OrganizationSearchResource(mixins.ListModelMixin, viewsets.GenericViewSet)
   filter_backends = (filters.OrderingFilter,)
   ordering_fields = ('slug', 'name', 'website', 'facebook_page', 'details', 'description', 'type', 'hidden_address')
 
+  def get_cache_key(self):
+    return 'organizations-{}-{}'.format(self.request.channel, hash(frozenset(self.request.GET.items())))
+
+  @cached
   def get_queryset(self):
     params = self.request.GET
 
-    key = 'organizations-{}-{}'.format(self.request.channel, hash(frozenset(params.items())))
-    cache_ttl = 120
-    result = cache.get(key)
+    highlighted = params.get('highlighted') == 'true'
+    published = params.get('published', 'true')
 
-    if not result:
-      highlighted = params.get('highlighted') == 'true'
-      published = params.get('published', 'true')
+    query = params.get('query', None)
+    cause = params.get('cause', None)
+    address = params.get('address', None)
+    name = params.get('name', None)
 
-      query = params.get('query', None)
-      cause = params.get('cause', None)
-      address = params.get('address', None)
-      name = params.get('name', None)
+    queryset = SearchQuerySet().models(Organization)
+    queryset = queryset.filter(highlighted=1) if highlighted else queryset
+    queryset = queryset.filter(content=query) if query else queryset
+    queryset = filters.by_name(queryset, name) if name else queryset
+    queryset = filters.by_published(queryset, published)
+    queryset = filters.by_address(queryset, address) if address else queryset
+    queryset = filters.by_causes(queryset, cause) if cause else queryset
+    queryset = queryset.filter(channel=self.request.channel)
 
-      queryset = SearchQuerySet().models(Organization)
-      queryset = queryset.filter(highlighted=1) if highlighted else queryset
-      queryset = queryset.filter(content=query) if query else queryset
-      queryset = filters.by_name(queryset, name) if name else queryset
-      queryset = filters.by_published(queryset, published)
-      queryset = filters.by_address(queryset, address) if address else queryset
-      queryset = filters.by_causes(queryset, cause) if cause else queryset
-      queryset = queryset.filter(channel=self.request.channel)
-
-      result_keys = [q.pk for q in queryset]
-      result = Organization.objects.filter(pk__in=result_keys, deleted=False).prefetch_related('causes').select_related('address').order_by('-highlighted')
-      result = filters.filter_out(result, "FILTER_OUT_ORGANIZATIONS", self.request.channel)
-      cache.set(key, result, cache_ttl)
+    result_keys = [q.pk for q in queryset]
+    result = Organization.objects.filter(pk__in=result_keys, deleted=False).prefetch_related('causes').select_related('address').order_by('-highlighted')
+    result = filters.filter_out(result, "FILTER_OUT_ORGANIZATIONS", self.request.channel)
 
     return result
 
@@ -78,53 +77,51 @@ class ProjectSearchResource(BookmarkAnnotationMixin, mixins.ListModelMixin, view
 
     return queryset
 
+  def get_cache_key(self):
+    return 'projects-{}-{}-{}'.format(self.request.channel, self.get_user_cache_id(), hash(frozenset(self.request.GET.items())))
+
+  @cached
   def get_queryset(self):
     params = self.request.GET
 
-    key = 'projects-{}-{}-{}'.format(self.request.channel, self.get_user_cache_id(), hash(frozenset(params.items())))
-    cache_ttl = 120
-    result = cache.get(key)
+    query = params.get('query', None)
+    cause = params.get('cause', None)
+    skill = params.get('skill', None)
+    category = params.get('category', None)
+    address = params.get('address', None)
+    highlighted = (params.get('highlighted') == 'true')
+    name = params.get('name', None)
+    published = params.get('published', 'true')
+    organization = params.get('organization', None)
+    not_organization = params.get('not_organization', None)
+    disponibility = params.get('disponibility', None)
+    date = params.get('date', None)
 
-    if not result:
-      query = params.get('query', None)
-      cause = params.get('cause', None)
-      skill = params.get('skill', None)
-      category = params.get('category', None)
-      address = params.get('address', None)
-      highlighted = (params.get('highlighted') == 'true')
-      name = params.get('name', None)
-      published = params.get('published', 'true')
-      organization = params.get('organization', None)
-      not_organization = params.get('not_organization', None)
-      disponibility = params.get('disponibility', None)
-      date = params.get('date', None)
+    queryset = SearchQuerySet().models(Project)
+    queryset = queryset.filter(highlighted=1) if highlighted else queryset
+    queryset = queryset.filter(content=query) if query else queryset
+    queryset = filters.by_published(queryset, published)
+    queryset = filters.by_address(queryset, address, project=True)
+    queryset = filters.by_name(queryset, name)
+    queryset = filters.by_skills(queryset, skill)
+    queryset = filters.by_causes(queryset, cause)
+    queryset = filters.by_categories(queryset, category)
+    queryset = filters.by_disponibility(queryset, disponibility)
+    queryset = filters.by_date(queryset, date)
+    queryset = queryset.filter(channel=self.request.channel)
 
-      queryset = SearchQuerySet().models(Project)
-      queryset = queryset.filter(highlighted=1) if highlighted else queryset
-      queryset = queryset.filter(content=query) if query else queryset
-      queryset = filters.by_published(queryset, published)
-      queryset = filters.by_address(queryset, address, project=True)
-      queryset = filters.by_name(queryset, name)
-      queryset = filters.by_skills(queryset, skill)
-      queryset = filters.by_causes(queryset, cause)
-      queryset = filters.by_categories(queryset, category)
-      queryset = filters.by_disponibility(queryset, disponibility)
-      queryset = filters.by_date(queryset, date)
-      queryset = queryset.filter(channel=self.request.channel)
+    result_keys = [q.pk for q in queryset]
 
-      result_keys = [q.pk for q in queryset]
+    result = self.get_base_queryset().filter(pk__in=result_keys)
 
-      result = self.get_base_queryset().filter(pk__in=result_keys)
+    if not_organization:
+      org = [o for o in not_organization.split(',')]
+      result = result.exclude(organization__in=org)
+    elif organization:
+      org = [o for o in organization.split(',')]
+      result = result.filter(organization__in=org)
 
-      if not_organization:
-        org = [o for o in not_organization.split(',')]
-        result = result.exclude(organization__in=org)
-      elif organization:
-        org = [o for o in organization.split(',')]
-        result = result.filter(organization__in=org)
-
-      result = filters.filter_out(result, "FILTER_OUT_PROJECTS", self.request.channel)
-      cache.set(key, result, cache_ttl)
+    result = filters.filter_out(result, "FILTER_OUT_PROJECTS", self.request.channel)
 
     return result
 
@@ -143,28 +140,27 @@ class UserSearchResource(mixins.ListModelMixin, viewsets.GenericViewSet):
     if int(get_channel_setting(channel, "ENABLE_USER_SEARCH")[0]) == 0:
       raise PermissionDenied
 
+  def get_cache_key(self):
+    return 'users-{}-{}'.format(self.request.channel, hash(frozenset(self.request.GET.items())))
+
+  @cached
   def get_queryset(self):
     params = self.request.GET
-    key = 'users-{}-{}'.format(self.request.channel, hash(frozenset(params.items())))
-    cache_ttl = 120
-    result = cache.get(key)
 
-    if not result:
-      cause = params.get('cause', None)
-      skill = params.get('skill', None)
-      name = params.get('name', None)
+    cause = params.get('cause', None)
+    skill = params.get('skill', None)
+    name = params.get('name', None)
 
-      queryset = SearchQuerySet().models(User)
-      queryset = filters.by_skills(queryset, skill)
-      queryset = filters.by_causes(queryset, cause)
-      queryset = filters.by_name(queryset, name)
-      queryset = queryset.filter(channel=self.request.channel)
+    queryset = SearchQuerySet().models(User)
+    queryset = filters.by_skills(queryset, skill)
+    queryset = filters.by_causes(queryset, cause)
+    queryset = filters.by_name(queryset, name)
+    queryset = queryset.filter(channel=self.request.channel)
 
-      result_keys = [q.pk for q in queryset]
-      related_field_name = get_profile_model()._meta.get_field('user').related_query_name()
+    result_keys = [q.pk for q in queryset]
+    related_field_name = get_profile_model()._meta.get_field('user').related_query_name()
 
-      result = User.objects.filter(pk__in=result_keys, public=True).prefetch_related(related_field_name + '__skills', related_field_name + '__causes').select_related(related_field_name)
-      cache.set(key, result, cache_ttl)
+    result = User.objects.filter(pk__in=result_keys, public=True).prefetch_related(related_field_name + '__skills', related_field_name + '__causes').select_related(related_field_name)
 
     return result
 
