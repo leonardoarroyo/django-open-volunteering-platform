3cab33f963afa3e870255d5b1f6f979787ec2755
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-09-25 18:58:49 -0300

    Basic Catalogue, Section and SectionFilter models with admin

--- ovp/apps/catalogue/admin.py
+++ ovp/apps/catalogue/admin.py
@@ -1,3 +1,41 @@
-from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from ovp.apps.channels.admin import admin_site
+from ovp.apps.channels.admin import ChannelModelAdmin
+from ovp.apps.channels.admin import CompactInline
+
+from ovp.apps.catalogue.models import Catalogue
+from ovp.apps.catalogue.models import Section
+from ovp.apps.catalogue.models import SectionFilter
+
+
+class SectionInline(CompactInline):
+  model = Section
+  fields = ["name", "slug"]
+  show_change_link = True
+
+class SectionFilterInline(CompactInline):
+  model = SectionFilter
+  fields = ["section", "type"]
+  show_change_link = True
+
+class CatalogueAdmin(ChannelModelAdmin):
+  fields = ["name", "slug"]
+  list_display = ["name", "slug"]
+  search_fields = ["id", "name", "slug"]
+  inlines = [SectionInline]
+
+class SectionAdmin(ChannelModelAdmin):
+  fields = ["name", "slug", "catalogue"]
+  list_display = ["name", "slug", "catalogue"]
+  search_fields = ["id", "name", "slug", "catalogue__name", "catalogue__slug"]
+  inlines = [SectionFilterInline]
+
+class SectionFilterAdmin(ChannelModelAdmin):
+  fields = ["section", "type"]
+  list_display = ["section", "type"]
+  search_fields = []
+
+
+admin_site.register(Catalogue, CatalogueAdmin)
+admin_site.register(Section, SectionAdmin)
--- ovp/apps/catalogue/models.py
+++ ovp/apps/catalogue/models.py
@@ -1,15 +1,29 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import ugettext_lazy as _
 
 from ovp.apps.channels.models.abstract import ChannelRelationship
 
+FILTER_TYPES = (
+  ("CATEGORY", "Category"),
+)
+
 class Catalogue(ChannelRelationship):
   name = models.CharField(_("Name"), max_length=100)
+  slug = models.SlugField(_("Slug"), max_length=100)
+
+  def __str__(self):
+    return self.name
 
 class Section(ChannelRelationship):
   catalogue = models.ForeignKey("catalogue.Catalogue")
   name = models.CharField(_("Name"), max_length=100)
-  slug = models.CharField(_("Slug"), max_length=100)
+  slug = models.SlugField(_("Slug"), max_length=100)
+
+  def __str__(self):
+    return self.name
 
 class SectionFilter(ChannelRelationship):
   section = models.ForeignKey("catalogue.Section")
+  type = models.CharField(_("Filter type"), max_length=30, choices=FILTER_TYPES)
