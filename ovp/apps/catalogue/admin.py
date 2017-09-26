54df53b05f1830612cd0eaf3ea810047c9c155f7
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-09-26 17:19:43 -0300

    Add filter object on admin

--- ovp/apps/catalogue/admin.py
+++ ovp/apps/catalogue/admin.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 
 from ovp.apps.channels.admin import admin_site
@@ -8,17 +9,42 @@ from ovp.apps.catalogue.models import Catalogue
 from ovp.apps.catalogue.models import Section
 from ovp.apps.catalogue.models import SectionFilter
 
+from ovp.apps.catalogue.models import CategoryFilter
 
+
+##################################
+# Mixins                         #
+##################################
+class FilterObjectMixin():
+  def filter_object(self, obj):
+    if obj.filter:
+      url = reverse('admin:%s_%s_change' %(obj.filter._meta.app_label,  obj.filter._meta.model_name),  args=[obj.filter.id])
+      return '<a href="%s">Edit</a>' % url
+    else:
+      return _('No filter')
+  filter_object.allow_tags = True
+
+
+
+##################################
+# Inlines                        #
+##################################
 class SectionInline(CompactInline):
   model = Section
   fields = ["name", "slug"]
   show_change_link = True
 
-class SectionFilterInline(CompactInline):
+class SectionFilterInline(FilterObjectMixin, CompactInline):
   model = SectionFilter
-  fields = ["section", "type"]
+  fields = ["section", "type", "filter_object"]
+  readonly_fields = ["filter_object"]
   show_change_link = True
 
+
+
+##################################
+# Admin                          #
+##################################
 class CatalogueAdmin(ChannelModelAdmin):
   fields = ["name", "slug"]
   list_display = ["name", "slug"]
@@ -31,11 +57,24 @@ class SectionAdmin(ChannelModelAdmin):
   search_fields = ["id", "name", "slug", "catalogue__name", "catalogue__slug"]
   inlines = [SectionFilterInline]
 
-class SectionFilterAdmin(ChannelModelAdmin):
-  fields = ["section", "type"]
+class SectionFilterAdmin(FilterObjectMixin, ChannelModelAdmin):
+  fields = ["section", "type", "filter_object"]
   list_display = ["section", "type"]
+  readonly_fields = ["filter_object"]
   search_fields = []
 
 
 admin_site.register(Catalogue, CatalogueAdmin)
 admin_site.register(Section, SectionAdmin)
+admin_site.register(SectionFilter, SectionFilterAdmin)
+
+
+##################################
+# Filters admin                  #
+##################################
+class CategoryFilterAdmin(ChannelModelAdmin):
+  fields = []
+  list_display = []
+  search_fields = []
+
+admin_site.register(CategoryFilter, CategoryFilterAdmin)
--- ovp/apps/catalogue/models/__init__.py
+++ ovp/apps/catalogue/models/__init__.py
@@ -1,3 +1,4 @@
 from .catalogue import Catalogue
 from .section import Section
 from .section import SectionFilter
+from .filter import CategoryFilter
