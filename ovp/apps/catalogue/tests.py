cedec97249e0f9bb90537aa3a8e43d5af588e137
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-10-03 11:58:09 -0300

    Create catalogue fetching and views

--- ovp/apps/catalogue/tests.py
+++ ovp/apps/catalogue/tests.py
@@ -1,17 +1,29 @@
 from django.test import TestCase
-
 from django.core.cache import cache
+from django.db.models.query import QuerySet
 
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
+from rest_framework.utils.serializer_helpers import ReturnList
+
+from ovp.apps.users.models import User
+
+from ovp.apps.projects.models import Project
 from ovp.apps.projects.models import Category
+from ovp.apps.projects.serializers.project import ProjectSearchSerializer
 
 from ovp.apps.catalogue.cache import get_catalogue
+from ovp.apps.catalogue.cache import fetch_catalogue
 from ovp.apps.catalogue.models import Catalogue
 from ovp.apps.catalogue.models import Section
 from ovp.apps.catalogue.models import SectionFilter
 
 def setUp():
+  # Categories
   category1 = Category.objects.create(name="Hot", object_channel="default")
+  category2 = Category.objects.create(name="Get your hands dirty", object_channel="default")
 
+  # Catalogue
   catalogue = Catalogue.objects.create(name="Home", slug="home", object_channel="default")
   section1 = Section.objects.create(name="Hot", slug="hot", catalogue=catalogue, object_channel="default")
   section1_filter = SectionFilter.objects.create(section=section1, type="CATEGORY", object_channel="default")
@@ -20,13 +32,21 @@ def setUp():
   section2 = Section.objects.create(name="Get your hands dirty", slug="get-your-hands-dirty", catalogue=catalogue, object_channel="default")
   section3 = Section.objects.create(name="This week", slug="this-week", catalogue=catalogue, object_channel="default")
 
+  # Projects
+  user = User.objects.create(email="sample@user.com", password="sample-user", object_channel="default")
+  project1 = Project.objects.create(name="sample 1", owner=user, description="description", details="detail", object_channel="default")
+  project1.categories.add(category1)
+
+  project2 = Project.objects.create(name="sample 2", owner=user, description="description", details="detail", object_channel="default")
+  project2.categories.add(category2)
+
   cache.clear()
 
-class CatalogueTestCase(TestCase):
+class CatalogueCacheTestCase(TestCase):
   def setUp(self):
     setUp()
 
-  def test_catalogue_caching(self):
+  def test_get_catalogue_caching(self):
     with self.assertNumQueries(5):
       # 4 from catalogue, section and section filters models
       # 1 from applied filters
@@ -37,9 +57,38 @@ class CatalogueTestCase(TestCase):
       catalogue2 = get_catalogue("default", "home")
     self.assertEqual(catalogue, catalogue2)
 
+  def test_fetch_queryset_without_serializer(self):
+    catalogue = get_catalogue("default", "home")
+    fetched = fetch_catalogue(catalogue)
+
+    self.assertEqual(fetched["sections"][0]["projects"].__class__, QuerySet)
+
+  def test_fetch_queryset_with_serializer(self):
+    catalogue = get_catalogue("default", "home")
+    fetched = fetch_catalogue(catalogue, serializer=ProjectSearchSerializer)
+
+    self.assertEqual(fetched["sections"][0]["projects"].__class__, ReturnList)
+
+class CatalogueViewTestCase(TestCase):
+  def setUp(self):
+    setUp()
+    self.client = APIClient()
+
+  def test_view_404(self):
+    response = self.client.get(reverse("catalogue", ["invalid"]), format="json")
+    self.assertEqual(response.status_code, 404)
+
+  def test_view_200(self):
+    response = self.client.get(reverse("catalogue", ["home"]), format="json")
+    self.assertEqual(response.status_code, 200)
+
 class CategoryFilterTestCase(TestCase):
   def setUp(self):
     setUp()
+    self.client = APIClient()
 
   def test_category_filter(self):
-    pass
+    response = self.client.get(reverse("catalogue", ["home"]), format="json")
+    import pudb;pudb.set_trace()
+
+# TODO: Assert query amount
--- ovp/apps/catalogue/views.py
+++ ovp/apps/catalogue/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import views
+from rest_framework import response
 
-# Create your views here.
+from ovp.apps.projects.serializers.project import ProjectSearchSerializer
+
+from ovp.apps.channels.viewsets.decorators import ChannelViewSet
+
+from ovp.apps.catalogue.cache import get_catalogue
+from ovp.apps.catalogue.cache import fetch_catalogue
+
+@ChannelViewSet
+class CatalogueView(views.APIView):
+  def get(self, request, slug):
+    catalogue = get_catalogue(request.channel, slug)
+    if not catalogue:
+      return response.Response({"detail": "This catalog does not exist"}, status=404)
+
+    fetched = fetch_catalogue(catalogue, serializer=ProjectSearchSerializer)
+
+    return response.Response(fetched)
