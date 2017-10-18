122776ed10958cad7fd8a119d7f2c92bbce9835f
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-10-18 17:00:27 -0200

    Pass context to catalogue serializer

--- ovp/apps/catalogue/views.py
+++ ovp/apps/catalogue/views.py
@@ -1,4 +1,4 @@
-from rest_framework import views
+from rest_framework import generics
 from rest_framework import response
 
 from ovp.apps.projects.serializers.project import ProjectSearchSerializer
@@ -9,12 +9,15 @@ from ovp.apps.catalogue.cache import get_catalogue
 from ovp.apps.catalogue.cache import fetch_catalogue
 
 @ChannelViewSet
-class CatalogueView(views.APIView):
+class CatalogueView(generics.GenericAPIView):
   def get(self, request, slug):
     catalogue = get_catalogue(request.channel, slug)
     if not catalogue:
       return response.Response({"detail": "This catalog does not exist"}, status=404)
 
-    fetched = fetch_catalogue(catalogue, serializer=ProjectSearchSerializer, request=self.request)
+    fetched = fetch_catalogue(catalogue,
+        serializer=ProjectSearchSerializer,
+        request=self.request,
+        context=self.get_serializer_context())
 
     return response.Response(fetched)
