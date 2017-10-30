98559b4c0df4ba81fab72cb130eafb4da45d9e2b
Fabio Garcia Salgado <fabioo.salgado@gmail.com>
2017-10-30 11:03:18 -0200

    Fix channel admin field in category faq

--- ovp/apps/faq/admin/category.py
+++ ovp/apps/faq/admin/category.py
@@ -5,7 +5,8 @@ from ovp.apps.channels.admin import ChannelModelAdmin
 from ovp.apps.faq.models import FaqCategory
 
 class FaqCategoryAdmin(ChannelModelAdmin):
-	list_display = ['id', 'name']
+  list_display = ['id', 'name']
+  fields = ['name']
 
 
 admin_site.register(FaqCategory, FaqCategoryAdmin)
