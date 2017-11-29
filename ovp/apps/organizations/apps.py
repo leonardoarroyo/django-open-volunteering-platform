ac63edcaf9a309ae05dd851e3e897e10aa48565d
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-11-29 13:57:14 -0200

    Localize app names

--- ovp/apps/organizations/apps.py
+++ ovp/apps/organizations/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
-
+from django.utils.translation import ugettext_lazy as _
 
 class OrganizationsConfig(AppConfig):
-    name = 'organizations'
+  name = 'ovp.apps.organizations'
+  verbose_name = _('Organizations')
