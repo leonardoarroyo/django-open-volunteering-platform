0a78c665ec08eb1e3a817f2b6efed8e777c7371c
Fabio Garcia <fabioo.salgado@gmail.com>
2018-05-04 16:31:12 -0300

    Change project address relationship

--- ovp/apps/projects/models/project.py
+++ ovp/apps/projects/models/project.py
@@ -23,7 +23,7 @@ class Project(ChannelRelationship):
   Project model
   """
   image = models.ForeignKey('uploads.UploadedImage', blank=True, null=True, verbose_name=_('image'))
-  address = models.OneToOneField('core.GoogleAddress', blank=True, null=True, verbose_name=_('address'), db_constraint=False)
+  address = models.ForeignKey('core.GoogleAddress', blank=True, null=True, verbose_name=_('address'), db_constraint=False)
   skills = models.ManyToManyField('core.Skill', verbose_name=_('skills'))
   causes = models.ManyToManyField('core.Cause', verbose_name=_('causes'))
   commentaries = models.ManyToManyField('core.Commentary', verbose_name=_('commentaries'))
