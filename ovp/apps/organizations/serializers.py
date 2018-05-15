13925de8e9b4d2623aa0e07f656d8d902090f4ac
Fabio Garcia <fabioo.salgado@gmail.com>
2018-05-15 14:02:05 -0300

    Remove document from organization serializer

--- ovp/apps/organizations/serializers.py
+++ ovp/apps/organizations/serializers.py
@@ -29,7 +29,7 @@ class OrganizationCreateSerializer(ChannelRelationshipSerializer):
 
   class Meta:
     model = models.Organization
-    fields = ['id', 'slug', 'owner', 'name', 'website', 'facebook_page', 'address', 'details', 'description', 'type', 'image', 'image_id', 'cover', 'hidden_address', 'causes', 'contact_name', 'contact_email', 'contact_phone', 'document']
+    fields = ['id', 'slug', 'owner', 'name', 'website', 'facebook_page', 'address', 'details', 'description', 'type', 'image', 'image_id', 'cover', 'hidden_address', 'causes', 'contact_name', 'contact_email', 'contact_phone']
 
   def create(self, validated_data):
     causes = validated_data.pop('causes', [])
