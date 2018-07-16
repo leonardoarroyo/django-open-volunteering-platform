5b5df30476f04f6f7d4919cbc43f2e366f135038
Leonardo Arroyo <contato@leonardoarroyo.com>
2018-07-16 12:27:23 -0300

    Fix data leak on public user

--- ovp/apps/projects/serializers/apply_user.py
+++ ovp/apps/projects/serializers/apply_user.py
@@ -10,21 +10,25 @@ from ovp.apps.organizations.serializers import OrganizationSearchSerializer
 address_serializers = get_address_serializers()
 
 """ Serializers """
+
+
 class ProjectApplyRetrieveSerializer(ChannelRelationshipSerializer):
-  image = UploadedImageSerializer()
-  address = address_serializers[1]()
-  organization = OrganizationSearchSerializer()
+    image = UploadedImageSerializer()
+    address = address_serializers[1]()
+    organization = OrganizationSearchSerializer()
 
-  class Meta:
-    model = models.Project
-    fields = ['slug', 'image', 'name', 'description', 'highlighted', 'published_date', 'address', 'details', 'created_date', 'organization', 'minimum_age', 'applied_count', 'max_applies', 'max_applies_from_roles', 'closed', 'closed_date', 'published', 'hidden_address', 'crowdfunding', 'public_project']
+    class Meta:
+        model = models.Project
+        fields = ['slug', 'image', 'name', 'description', 'highlighted', 'published_date', 'address', 'details', 'created_date', 'organization', 'minimum_age',
+                  'applied_count', 'max_applies', 'max_applies_from_roles', 'closed', 'closed_date', 'published', 'hidden_address', 'crowdfunding', 'public_project']
 
-  def to_representation(self, instance):
-    return super(ProjectApplyRetrieveSerializer, self).to_representation(instance)
+    def to_representation(self, instance):
+        return super(ProjectApplyRetrieveSerializer, self).to_representation(instance)
+
 
 class ApplyUserRetrieveSerializer(ChannelRelationshipSerializer):
-	project = ProjectApplyRetrieveSerializer()
+    project = ProjectApplyRetrieveSerializer()
 
-	class Meta:
-		model = models.Apply
-		fields = ['id', 'email', 'username', 'phone', 'date', 'canceled', 'canceled_date', 'project']
+    class Meta:
+        model = models.Apply
+        fields = ['id', 'date', 'canceled', 'canceled_date', 'project']
