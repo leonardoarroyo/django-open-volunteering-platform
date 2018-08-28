ac6520145bd03cc247a3ac8ad44f84319908ec44
Leonardo Arroyo <contato@leonardoarroyo.com>
2018-08-28 15:13:31 -0300

    Remove PV indicators from admin

--- ovp/apps/admin/jet/modules.py
+++ ovp/apps/admin/jet/modules.py
@@ -8,7 +8,6 @@ from django.db.models import Q
 from ovp.apps.organizations.models import Organization
 from ovp.apps.projects.models import Project, Apply
 from ovp.apps.users.models import User
-# from channels.pv.models import PVUserInfo
 
 class OVPRecentActions(RecentActions):
   def init_with_context(self, context):
@@ -61,14 +60,12 @@ class Indicators(DashboardModule):
     def init_with_context(self, context):
       organizations = Organization.objects.filter(deleted=False)
       projects = Project.objects.filter(deleted=False)
-      #able_to_apply = PVUserInfo.objects.filter(can_apply=True)
 
       self.organizations_count = organizations.count()
       self.organizations_published_count = organizations.filter(published=True).count()
       self.projects_count = projects.count()
       self.projects_published_count = projects.filter(published=True).count()
       self.users_count = User.objects.count()
-      #self.users_can_apply_count = able_to_apply.count()
       self.applies_count = Apply.objects.count()
 
       # Monthly
@@ -83,6 +80,4 @@ class Indicators(DashboardModule):
       self.month_projects_count = month_projects.count()
       self.month_projects_published_count = month_projects.filter(published=True).count()
       self.month_users_count = month_users.count()
-      #self.month_users_can_apply_count = month_users.filter(pvuserinfo__in=able_to_apply).count()
-      #self.month_applies_count = Apply.objects.filter(date__gte=day_one_month_before).count()
-
+      self.month_applies_count = Apply.objects.filter(date__gte=day_one_month_before).count()
