ee341bdc6716c905991922093611535da69ea56e
Fabio Garcia <fabioo.salgado@gmail.com>
2018-10-09 14:21:43 -0300

    Add disponibilty to project bookmarked serializer

--- ovp/apps/projects/serializers/bookmark_user.py
+++ ovp/apps/projects/serializers/bookmark_user.py
@@ -5,6 +5,7 @@ from ovp.apps.channels.serializers import ChannelRelationshipSerializer
 from ovp.apps.core.helpers import get_address_serializers
 from ovp.apps.uploads.serializers import UploadedImageSerializer
 from ovp.apps.organizations.serializers import OrganizationSearchSerializer
+from ovp.apps.projects.serializers.disponibility import DisponibilitySerializer
 
 """ Address serializers """
 address_serializers = get_address_serializers()
@@ -14,10 +15,11 @@ class ProjectBookmarkRetrieveSerializer(ChannelRelationshipSerializer):
   image = UploadedImageSerializer()
   address = address_serializers[1]()
   organization = OrganizationSearchSerializer()
+  disponibility = DisponibilitySerializer()
 
   class Meta:
     model = models.Project
-    fields = ['slug', 'image', 'name', 'description', 'highlighted', 'published_date', 'address', 'created_date', 'organization', 'minimum_age', 'applied_count', 'max_applies', 'max_applies_from_roles', 'closed', 'closed_date', 'published', 'hidden_address', 'crowdfunding', 'public_project']
+    fields = ['slug', 'image', 'name', 'description', 'highlighted', 'published_date', 'address', 'created_date', 'organization', 'minimum_age', 'applied_count', 'max_applies', 'max_applies_from_roles', 'closed', 'closed_date', 'published', 'hidden_address', 'crowdfunding', 'public_project', 'disponibility']
 
 class BookmarkUserRetrieveSerializer(ChannelRelationshipSerializer):
   project = ProjectBookmarkRetrieveSerializer()
