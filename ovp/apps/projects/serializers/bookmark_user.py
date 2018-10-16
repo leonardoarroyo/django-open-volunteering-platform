718b66ffe5e3d348098838927958deb945a1b6fe
Fabio Garcia <fabioo.salgado@gmail.com>
2018-10-16 12:08:16 -0300

    Add disponibilty representation in project bookmark serializer

--- ovp/apps/projects/serializers/bookmark_user.py
+++ ovp/apps/projects/serializers/bookmark_user.py
@@ -5,7 +5,7 @@ from ovp.apps.channels.serializers import ChannelRelationshipSerializer
 from ovp.apps.core.helpers import get_address_serializers
 from ovp.apps.uploads.serializers import UploadedImageSerializer
 from ovp.apps.organizations.serializers import OrganizationSearchSerializer
-from ovp.apps.projects.serializers.disponibility import DisponibilitySerializer
+from ovp.apps.projects.serializers.disponibility import DisponibilitySerializer, add_disponibility_representation
 
 """ Address serializers """
 address_serializers = get_address_serializers()
@@ -20,6 +20,10 @@ class ProjectBookmarkRetrieveSerializer(ChannelRelationshipSerializer):
   class Meta:
     model = models.Project
     fields = ['slug', 'image', 'name', 'description', 'highlighted', 'published_date', 'address', 'created_date', 'organization', 'minimum_age', 'applied_count', 'max_applies', 'max_applies_from_roles', 'closed', 'closed_date', 'published', 'hidden_address', 'crowdfunding', 'public_project', 'disponibility']
+
+  @add_disponibility_representation
+  def to_representation(self, instance):
+    return super(ProjectBookmarkRetrieveSerializer, self).to_representation(instance)
 
 class BookmarkUserRetrieveSerializer(ChannelRelationshipSerializer):
   project = ProjectBookmarkRetrieveSerializer()
