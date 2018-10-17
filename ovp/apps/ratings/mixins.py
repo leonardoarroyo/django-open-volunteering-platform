25716208629ccdebf22aebb5ad3eff9bdaa71447
Leonardo Arroyo <contato@leonardoarroyo.com>
2018-10-17 18:11:23 -0300

    Add RatingRequest

--- ovp/apps/ratings/mixins.py
+++ ovp/apps/ratings/mixins.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 class RatedModelMixin(models.Model):
-  rating_parameters = models.ManyToManyField('ratings.RatingParameter')
   ratings = models.ManyToManyField('ratings.Rating')
   rating = models.FloatField(_('Rating'), null=True, default=None)
   
