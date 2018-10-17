1799d7f9bb9d0619a1c20e8b05b4d1da44b1303e
Leonardo Arroyo <contato@leonardoarroyo.com>
2018-10-17 17:31:47 -0300

    Start rating models

--- ovp/apps/ratings/models/rating.py
+++ ovp/apps/ratings/models/rating.py
@@ -1,2 +1,22 @@
 from django.db import models
-pass
+from django.utils.translation import ugettext_lazy as _
+
+POSSIBLE_TYPES = (
+  (1, "Qualitative"),
+  (2, "Quantitative"),
+)
+class RatingParameter(models.Model):
+  name = models.CharField(_('Name'), max_length=100)
+  description = models.TextField(_('Description'))
+  type = models.IntegerField(_('Parameter type'), choices=POSSIBLE_TYPES)
+
+class Rating(models.Model):
+  owner = models.ForeignKey('users.User')
+  created_date = models.DateTimeField(auto_now_add=True)
+  modified_date = models.DateTimeField(auto_now=True)
+
+class RatingAnswer(models.Model):
+  rating = models.ForeignKey('ratings.Rating')
+  parameter = models.ForeignKey('ratings.RatingParameter')
+  value_quantitative = models.FloatField('quantitative value', null=True)
+  value_qualitative = models.TextField('qualitative value', blank=True, null=True)
