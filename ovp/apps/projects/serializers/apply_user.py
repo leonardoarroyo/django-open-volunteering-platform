dafd4a3c9e971e355ec866cfba4b03dfd1d84e1a
Leonardo Arroyo <contato@leonardoarroyo.com>
2018-12-11 18:18:25 -0200

    Add rating to UserApplyRetrieveSerializer

--- ovp/apps/projects/serializers/apply_user.py
+++ ovp/apps/projects/serializers/apply_user.py
@@ -24,7 +24,7 @@ class ProjectApplyRetrieveSerializer(ChannelRelationshipSerializer):
     class Meta:
         model = models.Project
         fields = ['slug', 'image', 'name', 'description', 'highlighted', 'published_date', 'address', 'details', 'created_date', 'organization', 'minimum_age',
-                  'applied_count', 'max_applies', 'max_applies_from_roles', 'closed', 'closed_date', 'published', 'hidden_address', 'crowdfunding', 'public_project', 'disponibility']
+                  'applied_count', 'max_applies', 'max_applies_from_roles', 'closed', 'closed_date', 'published', 'hidden_address', 'crowdfunding', 'public_project', 'disponibility', 'rating']
 
     @add_disponibility_representation
     def to_representation(self, instance):
