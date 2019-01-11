de13239cc63d5beefe985037e339d447121d3408
Leonardo Arroyo <contato@leonardoarroyo.com>
2019-01-11 13:18:10 -0200

    Update indicators

--- ovp/apps/admin/jet/modules.py
+++ ovp/apps/admin/jet/modules.py
@@ -1,5 +1,6 @@
 import datetime
 from django.utils import timezone
+from django.utils.dateparse import parse_date
 from dateutil.relativedelta import relativedelta
 from jet.dashboard.modules import RecentActions
 from jet.dashboard.modules import DashboardModule
@@ -8,6 +9,8 @@ from django.db.models import Q
 from ovp.apps.organizations.models import Organization
 from ovp.apps.projects.models import Project, Apply
 from ovp.apps.users.models import User
+from django.db.models import Sum
+from django.conf import settings
 
 class OVPRecentActions(RecentActions):
   def init_with_context(self, context):
@@ -58,26 +61,53 @@ class Indicators(DashboardModule):
     template = "admin/indicators.html"
 
     def init_with_context(self, context):
-      organizations = Organization.objects.filter(deleted=False)
-      projects = Project.objects.filter(deleted=False)
+      date_min = context["request"].GET.get("indicator__gte", None)
+      date_max = context["request"].GET.get("indicator__lte", None)
 
-      self.organizations_count = organizations.count()
-      self.organizations_published_count = organizations.filter(published=True).count()
-      self.projects_count = projects.count()
-      self.projects_published_count = projects.filter(published=True).count()
-      self.users_count = User.objects.count()
-      self.applies_count = Apply.objects.count()
+      if date_min:
+        date_min = parse_date(date_min)
 
-      # Monthly
-      now = timezone.now()
-      day_one_month_before = (now - datetime.timedelta(365/12)).replace(tzinfo=timezone.utc)
-      month_organizations = organizations.filter(created_date__gte=day_one_month_before)
-      month_projects = projects.filter(created_date__gte=day_one_month_before)
-      month_users = User.objects.filter(joined_date__gte=day_one_month_before)
+      if date_max:
+        date_max = parse_date(date_max)
 
-      self.month_organizations_count = month_organizations.count()
-      self.month_organizations_published_count = month_organizations.filter(published=True).count()
-      self.month_projects_count = month_projects.count()
-      self.month_projects_published_count = month_projects.filter(published=True).count()
-      self.month_users_count = month_users.count()
-      self.month_applies_count = Apply.objects.filter(date__gte=day_one_month_before).count()
+      if date_min and date_max and date_max <= date_min:
+        tmp = date_min
+        date_min = date_max
+        date_max = tmp
+
+      organizations_created = self.date_filter(Organization.objects.filter(deleted=False), date_min, date_max, "created_date")
+      organizations_published = self.date_filter(Organization.objects.filter(deleted=False), date_min, date_max, "published_date")
+      projects_created = self.date_filter(Project.objects.filter(deleted=False), date_min, date_max, "created_date")
+      projects_published = self.date_filter(Project.objects.filter(deleted=False), date_min, date_max, "published_date")
+      users = self.date_filter(User.objects.all(), date_min, date_max, "joined_date")
+      applies = self.date_filter(Apply.objects.all(), date_min, date_max, "date")
+
+      apply_distinct_qs = Apply.objects.all()
+      if settings.DATABASES['default']['ENGINE'] != 'django.db.backends.sqlite3':
+        apply_distinct_qs = apply_distinct_qs.distinct('user')
+      applies_distinct = self.date_filter(apply_distinct_qs, date_min, date_max, "date")
+
+      self.organizations_count = organizations_created.count()
+      self.organizations_published_count = organizations_published.filter(published=True).count()
+      self.projects_count = projects_created.count()
+      self.projects_published_count = projects_published.filter(published=True).count()
+      self.users_count = users.count()
+      self.applies_count = applies.count()
+      self.applies_count_distinct = applies_distinct.count()
+      self.benefited_people_count = projects_published.aggregate(Sum('benefited_people'))["benefited_people__sum"]
+      if not self.benefited_people_count:
+        self.benefited_people_count = 0
+
+      self.date_min = date_min
+      self.date_max = date_max
+
+    def date_filter(self, qs, date_min, date_max, field=None):
+      filters = {}
+
+      if date_min:
+        filters["{}__gte".format(field)] = date_min
+
+      if date_max:
+        filters["{}__lte".format(field)] = date_max
+
+      return qs.filter(**filters)
