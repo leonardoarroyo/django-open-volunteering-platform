13d2735cd8fdedbff9d30358b07e5fc16311398b
Leonardo Arroyo <contato@leonardoarroyo.com>
2019-04-19 13:57:44 -0300

    Create donation backends

---
+++ ovp/apps/donations/views.py
@@ -0,0 +1,18 @@
+# POST /donate/
+# POST /subscribe/
+# GET /transactions/
+# GET /subscriptions/
+# DELETE /subscription/
+
+class DonationsViewSet(viewsets.GenericViewSet):
+  def donate():
+    pass
+
+  def subscribe():
+    pass
+
+  def transactions():
+    pass
+
+  def subscriptions():
+    pass
