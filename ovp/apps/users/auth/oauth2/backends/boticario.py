5ca877ac7724157c5db2ea736a9a85b1b9c2ad96
Yan Orestes <yanorestes@hotmail.com>
2019-05-31 16:48:37 -0300

    Added Boticario OAuth2 backend

---
+++ ovp/apps/users/auth/oauth2/backends/boticario.py
@@ -0,0 +1,30 @@
+from social_core.backends.oauth import BaseOAuth2
+from rest_framework.exceptions import AuthenticationFailed
+
+class BoticarioOAuth2(BaseOAuth2):
+    """Boticario OAuth2 authentication backend"""
+    name = "boticario"
+    AUTHORIZATION_URL = "https://auth.grupoboticario.com.br/adfs/oauth2/authorize/"
+    ACCESS_TOKEN_URL = "https://auth.grupoboticario.com.br/adfs/oauth2/token/"
+    REVOKE_TOKEN_URL = "https://auth.grupoboticario.com.br/adfs/oauth2/logout"
+    DEFAULT_SCOPE = ['openid', 'email', 'profile']
+
+    def auth_allowed(self, response, details):
+        if not response.get("email", False):
+            raise AuthenticationFailed({
+                "error": "access_denied",
+                "error_description": "Your email is not verified by the provider"
+            })
+        return super(BoticarioOAuth2, self).auth_allowed(response, details)
+
+    def get_user_details(self, response):
+        return {
+            'email': response.get('email'),
+            'name': response.get('fullname')
+        }
+
+    def user_data(self, access_token, *args, **kwargs):
+        url = "https://auth.grupoboticario.com.br/adfs/userinfo?" + urlencode({
+            'access_token': access_token
+        })
+        return self.get_json(url)
