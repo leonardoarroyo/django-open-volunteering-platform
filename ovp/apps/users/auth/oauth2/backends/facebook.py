130950aa8fab0ccd7f926e62303373e8c35c538b
Leonardo Arroyo <contato@leonardoarroyo.com>
2019-07-19 15:42:34 -0300

    Fix key loading

--- ovp/apps/users/auth/oauth2/backends/facebook.py
+++ ovp/apps/users/auth/oauth2/backends/facebook.py
@@ -21,7 +21,7 @@ class FacebookOAuth2(FacebookOAuth2Base):
 
   def get_key_and_secret(self):
     url = self.strategy.request.META['HTTP_HOST']
-    face_keys = json.loads(os.environ.get('FACEBOOK_KEYS', {}))
+    face_keys = json.loads(os.environ.get('FACEBOOK_KEYS', "{}"))
     for channel in face_keys:
       if channel in url:
         return tuple(face_keys[channel])
