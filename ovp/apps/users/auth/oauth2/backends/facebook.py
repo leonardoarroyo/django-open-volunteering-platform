490e06c03b8eae46f86de73fed0e3dbd9cc19a0d
Leonardo Arroyo <contato@leonardoarroyo.com>
2019-08-05 15:49:36 -0300

    Remove debug exception

--- ovp/apps/users/auth/oauth2/backends/facebook.py
+++ ovp/apps/users/auth/oauth2/backends/facebook.py
@@ -23,5 +23,4 @@ class FacebookOAuth2(FacebookOAuth2Base):
     url = self.strategy.request.META['HTTP_HOST']
     keys = json.loads(os.environ.get('FACEBOOK_KEYS', "{}"))
     key_tuple = tuple(keys.get(self.strategy.request.channel, (self.setting('KEY'), self.setting('SECRET'))))
-    raise Exception(key_tuple)
     return key_tuple
