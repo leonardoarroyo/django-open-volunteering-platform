f8a87333a06943ab406c064a5c76a468cca1cb3e
Levi Macario <levimacario@gmail.com>
2019-10-02 12:47:59 -0300

    PEP8 nos arquivos de ovp/apps/core/models/

--- ovp/apps/core/models/cause.py
+++ ovp/apps/core/models/cause.py
@@ -10,30 +10,57 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
 
+
 # Default causes
-causes = ['Professional Training', 'Fight Poverty', 'Conscious consumption', 'Culture, Sport and Art', 'Human Rights', 'Education', 'Youth', 'Elders', 'Environment', 'Citizen Participation', 'Animal Protection', 'Health', 'People with disabilities']
+CAUSES = [
+    'Professional Training',
+    'Fight Poverty',
+    'Conscious consumption',
+    'Culture, Sport and Art',
+    'Human Rights',
+    'Education',
+    'Youth',
+    'Elders',
+    'Environment',
+    'Citizen Participation',
+    'Animal Protection',
+    'Health',
+    'People with disabilities'
+]
+
 
 class Cause(ChannelRelationship):
-  name = models.CharField(_('name'), max_length=100)
-  image = models.ForeignKey('uploads.UploadedImage', blank=True, null=True, verbose_name=_('image'))
-  slug = models.SlugField(_('slug'), max_length=100, blank=True, null=True)
+    name = models.CharField(_('name'), max_length=100)
+    image = models.ForeignKey(
+        'uploads.UploadedImage',
+        blank=True,
+        null=True,
+        verbose_name=_('image')
+    )
+    slug = models.SlugField(_('slug'), max_length=100, blank=True, null=True)
 
-  class Meta:
-    app_label = 'core'
-    verbose_name = _('cause')
-    unique_together = (('slug', 'channel'), )
+    class Meta:
+        app_label = 'core'
+        verbose_name = _('cause')
+        unique_together = (('slug', 'channel'), )
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
-  def save(self, *args, **kwargs):
-    if not self.pk:
-      self.slug = generate_slug(Cause, self.name, kwargs.get("object_channel", None))
-    super(Cause, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = generate_slug(
+                Cause,
+                self.name,
+                kwargs.get("object_channel", None)
+            )
+        super().save(*args, **kwargs)
+
 
 @receiver(post_save, sender=Channel)
 def create_default_skills(sender, instance, **kwargs):
-  for cause in causes:
-    Cause.objects.create(name=cause, object_channel=instance.slug)
+    for cause in CAUSES:
+        Cause.objects.create(name=cause, object_channel=instance.slug)
+
 
 vinaigrette.register(Cause, ['name'])
--- ovp/apps/core/models/skill.py
+++ ovp/apps/core/models/skill.py
@@ -10,29 +10,50 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
 
+
 # Default skills
-skills = ['Arts/Handcrafting', 'Communication', 'Dance/Music', 'Law', 'Education', 'Sports', 'Cooking', 'Management', 'Idioms', 'Computers/Technology', 'Health', 'Others']
+SKILLS = [
+    'Arts/Handcrafting',
+    'Communication',
+    'Dance/Music',
+    'Law',
+    'Education',
+    'Sports',
+    'Cooking',
+    'Management',
+    'Idioms',
+    'Computers/Technology',
+    'Health',
+    'Others'
+]
+
 
 class Skill(ChannelRelationship):
-  name = models.CharField(_('name'), max_length=100)
-  slug = models.SlugField(_('slug'), max_length=100, blank=True, null=True)
+    name = models.CharField(_('name'), max_length=100)
+    slug = models.SlugField(_('slug'), max_length=100, blank=True, null=True)
 
-  class Meta:
-    app_label = 'core'
-    verbose_name = _('skill')
-    unique_together = (('slug', 'channel'),)
+    class Meta:
+        app_label = 'core'
+        verbose_name = _('skill')
+        unique_together = (('slug', 'channel'),)
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
-  def save(self, *args, **kwargs):
-    if not self.pk:
-      self.slug = generate_slug(Skill, self.name, kwargs.get("object_channel", None))
-    super(Skill, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = generate_slug(
+                Skill,
+                self.name,
+                kwargs.get("object_channel", None)
+            )
+        super().save(*args, **kwargs)
+
 
 @receiver(post_save, sender=Channel)
 def create_default_skills(sender, instance, **kwargs):
-  for skill in skills:
-    Skill.objects.create(name=skill, object_channel=instance.slug)
+    for skill in SKILLS:
+        Skill.objects.create(name=skill, object_channel=instance.slug)
+
 
 vinaigrette.register(Skill, ['name'])
