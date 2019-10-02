10231ba7a642e5fcccb0f3b7cd0a750b6acc21ce
Levi Macario <levimacario@gmail.com>
2019-10-02 12:22:52 -0300

    PEP8 nos arquivos de ovp/apps/core/models/address/

--- ovp/apps/core/models/address/simple_address.py
+++ ovp/apps/core/models/address/simple_address.py
@@ -1,19 +1,68 @@
 from django.db import models
 from ovp.apps.channels.models.abstract import ChannelRelationship
 
+
 class SimpleAddress(ChannelRelationship):
-  street = models.CharField(max_length=300, null=True, blank=True, verbose_name='Logradouro')
-  number = models.CharField(max_length=10, null=True, blank=True, verbose_name='Número') # May contain letters as well
-  neighbourhood = models.CharField(max_length=100, null=True, blank=True, verbose_name='Bairro')
-  city = models.CharField(max_length=100, null=True, blank=True, verbose_name='Cidade')
-  state = models.CharField(max_length=100, null=True, blank=True, verbose_name='Estado')
-  zipcode = models.CharField(max_length=20, null=True, blank=True, verbose_name='CEP')
-  country = models.CharField(max_length=100, null=True, blank=True, verbose_name='País')
-  supplement = models.CharField(max_length=100, null=True, blank=True, verbose_name='Complemento')
+    street = models.CharField(
+        max_length=300,
+        null=True,
+        blank=True,
+        verbose_name='Logradouro'
+    )
 
-  @staticmethod
-  def autocomplete_search_fields():
-    return 'street', 'city',
+    # May contain letters as well
+    number = models.CharField(
+        max_length=10,
+        null=True,
+        blank=True,
+        verbose_name='Número'
+    )
+    neighbourhood = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        verbose_name='Bairro'
+    )
+    city = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        verbose_name='Cidade'
+    )
+    state = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        verbose_name='Estado'
+    )
+    zipcode = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True,
+        verbose_name='CEP'
+    )
+    country = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        verbose_name='País'
+    )
+    supplement = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        verbose_name='Complemento'
+    )
 
-  def __str__(self):
-    return "{}, {} - {} - {}".format(self.street, self.number, self.neighbourhood, self.city)
+    @staticmethod
+    def autocomplete_search_fields():
+        return 'street', 'city',
+
+    def __str__(self):
+        params = {
+            'street': self.street,
+            'number': self.number,
+            'neighbourhood': self.neighbourhood,
+            'city': self.city
+        }
+        return "{street}, {number} - {neighbourhood} - {city}".format(**params)
