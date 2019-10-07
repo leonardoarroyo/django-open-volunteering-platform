49c5783fe39b5e0a79ace1eb5ceea1a3fe5e902b
Levi Macario <levimacario@gmail.com>
2019-10-07 10:56:27 -0300

    PEP8 nos arquivos de ovp/apps/galery/

--- ovp/apps/gallery/admin.py
+++ ovp/apps/gallery/admin.py
@@ -10,40 +10,52 @@ from ovp.apps.gallery.models import Gallery
 
 from martor.widgets import AdminMartorWidget
 
+
 class UploadedImageInline(TabularInline):
-  model = Gallery.images.through
-  exclude = ['channel']
-  readonly_fields = ['download_link']
-  verbose_name = "Image"
-  verbose_name_plural = "Images"
+    model = Gallery.images.through
+    exclude = ['channel']
+    readonly_fields = ['download_link']
+    verbose_name = "Image"
+    verbose_name_plural = "Images"
 
-  def download_link(self, instance):
-    url = instance.uploadedimage.image.url
-    return format_html('<a href="{}" target="_blank">Download</a>', url)
+    def download_link(self, instance):
+        url = instance.uploadedimage.image.url
+        return format_html('<a href="{}" target="_blank">Download</a>', url)
 
-class GalleryAdmin(ChannelModelAdmin):
-  fields = ['id', 'uuid', 'name',  'description', 'owner', 'images', ('created_date', 'modified_date')]
+    class GalleryAdmin(ChannelModelAdmin):
+        fields = [
+            'id',
+            'uuid',
+            'name',
+            'description',
+            'owner',
+            'images',
+            (
+                'created_date',
+                'modified_date'
+            )
+        ]
 
-  list_display = ['id', 'uuid', 'name']
+        list_display = ['id', 'uuid', 'name']
 
-  list_filter = []
+        list_filter = []
 
-  list_editable = []
+        list_editable = []
 
-  search_fields = ['id', 'content']
+        search_fields = ['id', 'content']
 
-  readonly_fields = ['id', 'uuid', 'created_date', 'modified_date']
+        readonly_fields = ['id', 'uuid', 'created_date', 'modified_date']
 
-  raw_id_fields = []
+        raw_id_fields = []
 
-  formfield_overrides = {
-    models.TextField: {'widget': AdminMartorWidget},
-  }
+        formfield_overrides = {
+            models.TextField: {'widget': AdminMartorWidget},
+        }
 
-  inlines = [UploadedImageInline]
+        inlines = [UploadedImageInline]
 
-  def post(self, obj):
-    return obj.__str__()
+        def post(self, obj):
+            return obj.__str__()
 
 
 admin_site.register(Gallery, GalleryAdmin)
--- ovp/apps/gallery/views.py
+++ ovp/apps/gallery/views.py
@@ -12,72 +12,96 @@ from rest_framework import viewsets
 from rest_framework import response
 from rest_framework import status
 
+
 @ChannelViewSet
-class GalleryResourceViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
-  lookup_field = "uuid"
+class GalleryResourceViewSet(mixins.CreateModelMixin,
+                             mixins.RetrieveModelMixin,
+                             viewsets.GenericViewSet):
 
-  def get_queryset(self, *args, **kwargs):
-    return Gallery.objects.filter(deleted=False)
+    lookup_field = "uuid"
 
-  def get_serializer_class(self, *args, **kwargs):
-    if self.action == "retrieve":
-      return GalleryRetrieveSerializer
-    if self.action == "create":
-      return GalleryCreateUpdateSerializer
-    if self.action == 'partial_update':
-      return GalleryCreateUpdateSerializer
+    def get_queryset(self, *args, **kwargs):
+        return Gallery.objects.filter(deleted=False)
 
-    return GalleryRetrieveSerializer
+    def get_serializer_class(self, *args, **kwargs):
+        if self.action == "retrieve":
+            return GalleryRetrieveSerializer
+        if self.action == "create":
+            return GalleryCreateUpdateSerializer
+        if self.action == 'partial_update':
+            return GalleryCreateUpdateSerializer
 
-  def get_permissions(self):
-    if self.action == 'create':
-      self.permission_classes = (permissions.IsAuthenticated,)
-    if self.action == 'partial_update':
-      self.permission_classes = (permissions.IsAuthenticated, GalleryEditAllowed)
-    if self.action == 'retrieve':
-      self.permission_classes = ()
-    if self.action == 'delete':
-      self.permission_classes = (permissions.IsAuthenticated, GalleryEditAllowed)
-    return super(GalleryResourceViewSet, self).get_permissions()
+        return GalleryRetrieveSerializer
 
-  ##################
-  # ViewSet routes #
-  ##################
-  def associate_with_model(self, *args, **kwargs):
-    pass
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = (permissions.IsAuthenticated,)
+        if self.action == 'partial_update':
+            self.permission_classes = (
+                permissions.IsAuthenticated,
+                GalleryEditAllowed
+            )
+        if self.action == 'retrieve':
+            self.permission_classes = ()
+        if self.action == 'delete':
+            self.permission_classes = (
+                permissions.IsAuthenticated,
+                GalleryEditAllowed
+            )
+        return super().get_permissions()
 
-  def unassociate_with_model(self, *args, **kwargs):
-    pass
+    ##################
+    # ViewSet routes #
+    ##################
+    def associate_with_model(self, *args, **kwargs):
+        pass
 
-  def partial_update(self, request, *args, **kwargs):
-    """ Partially update an gallery object. """
-    instance = self.get_object()
-    serializer = self.get_serializer(instance, data=request.data, partial=True, context=self.get_serializer_context())
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
+    def unassociate_with_model(self, *args, **kwargs):
+        pass
 
-    if getattr(instance, '_prefetched_objects_cache', None): #pragma: no cover
-      instance = self.get_object()
-      serializer = self.get_serializer(instance)
+    def partial_update(self, request, *args, **kwargs):
+        """ Partially update an gallery object. """
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance,
+            data=request.data,
+            partial=True,
+            context=self.get_serializer_context()
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-    return response.Response(serializer.data)
+        # pragma: no cover
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance = self.get_object()
+            serializer = self.get_serializer(instance)
 
-  def create(self, request, *args, **kwargs):
-    """ Create a gallery. """
-    request.data['owner'] = request.data.get('owner', None) or request.user.pk
+        return response.Response(serializer.data)
 
-    serializer = self.get_serializer(data=request.data, context=self.get_serializer_context())
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
+    def create(self, request, *args, **kwargs):
+        """ Create a gallery. """
+        owner_in_data = request.data.get('owner', None)
+        request.data['owner'] = owner_in_data or request.user.pk
 
-    headers = self.get_success_headers(serializer.data)
-    return response.Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        serializer = self.get_serializer(
+            data=request.data,
+            context=self.get_serializer_context()
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-  def delete(self, request, *args, **kwargs):
-    """ Delete a gallery. """
-    instance = self.get_object()
-    instance.deleted = True
-    instance.deleted_date = timezone.now()
-    instance.save()
+        headers = self.get_success_headers(serializer.data)
+        return response.Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
 
-    return response.Response({"deleted": True}, status=200)
+    def delete(self, request, *args, **kwargs):
+        """ Delete a gallery. """
+        instance = self.get_object()
+        instance.deleted = True
+        instance.deleted_date = timezone.now()
+        instance.save()
+
+        return response.Response({"deleted": True}, status=200)
