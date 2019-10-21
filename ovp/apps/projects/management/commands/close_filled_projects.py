d14c936a10fb1c43a3170c70b887a14e04d48cc3
Levi Macario <levimacario@gmail.com>
2019-10-21 10:12:40 -0300

    PEP8 em ovp/apps/projects/management/

--- ovp/apps/projects/management/commands/close_filled_projects.py
+++ ovp/apps/projects/management/commands/close_filled_projects.py
@@ -5,22 +5,24 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 from ovp.apps.projects.models import Project
 
+
 class Command(BaseCommand):
-  help = "Close projects that have been filled"
 
-  def handle(self, *args, **options):
-    projects = Project.objects.filter(closed=False)
+    help = "Close projects that have been filled"
 
-    for project in projects:
-      role_count = project.roles.count()
-      is_filled=True
+    def handle(self, *args, **options):
+        projects = Project.objects.filter(closed=False)
 
-      for role in project.roles.all():
-        if role.vacancies > role.apply_set.count():
-          is_filled=False
-          break
+        for project in projects:
+            role_count = project.roles.count()
+            is_filled = True
 
-      if is_filled:
-        print("Closing filled project {}".format(project.slug))
-        project.closed=True
-        project.save()
+        for role in project.roles.all():
+            if role.vacancies > role.apply_set.count():
+                is_filled = False
+                break
+
+        if is_filled:
+            print("Closing filled project {}".format(project.slug))
+            project.closed = True
+            project.save()
