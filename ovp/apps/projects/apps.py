f6d50414c98069034e331ee4553770c9496caf28
Levi Macario <levimacario@gmail.com>
2019-11-01 14:45:52 -0300

    PEP8 nos arquivos da raiz de ovp/apps/projects

--- ovp/apps/projects/apps.py
+++ ovp/apps/projects/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
+
 class ProjectsConfig(AppConfig):
-  name = 'ovp.apps.projects'
-  verbose_name = _('Projects')
+    name = 'ovp.apps.projects'
+    verbose_name = _('Projects')
