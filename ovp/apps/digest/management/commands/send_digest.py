e6b6fad5a0c84b5fb4303b7ae42b45bd7c6c3598
Leonardo Arroyo <contato@leonardoarroyo.com>
2019-11-04 03:00:52 -0300

    Campaign 4

--- ovp/apps/digest/management/commands/send_digest.py
+++ ovp/apps/digest/management/commands/send_digest.py
@@ -1,7 +1,14 @@
-from ovp.apps.digest.digest import get_email_list
-from ovp.apps.digest.digest import send_campaign
+from ovp.apps.digest.digest import ContentGenerator
+from ovp.apps.digest.digest import DigestCampaign
+from ovp.apps.digest.models import DigestLog
+from ovp.apps.digest.backends.aws import AWSBackend
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
   def handle(self, *args, **options):
-    send_campaign(chunk_size=1000)
+    campaign = DigestCampaign(backend=AWSBackend)
+    sent = set(DigestLog.objects.filter(campaign__gte=4).values_list('recipient', flat=True))
+    all_emails = campaign._get_email_list()
+    to_send = filter(lambda x: x not in sent, all_emails)
+    campaign.cg = ContentGenerator(threaded=False)
+    campaign.send_campaign(chunk_size=50, email_list=to_send)
