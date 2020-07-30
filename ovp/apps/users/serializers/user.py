6f33df7cdc5ec8b9a4cbbf484177f4627b7a6d83
Leonardo Arroyo <contato@leonardoarroyo.com>
2020-07-29 21:05:55 -0300

    Remove rating requests from current-user

--- ovp/apps/users/serializers/user.py
+++ ovp/apps/users/serializers/user.py
@@ -193,9 +193,6 @@ class CurrentUserSerializer(ChannelRelationshipSerializer):
     profile = get_profile_serializers()[1]()
     organizations = OrganizationOwnerRetrieveSerializer(
         many=True, source='active_organizations')
-    rating_requests_user_count = serializers.SerializerMethodField()
-    rating_requests_project_count = serializers.SerializerMethodField()
-    rating_requests_projects_with_unrated_users = serializers.SerializerMethodField()
     chat_enabled = serializers.SerializerMethodField()
     notifications_token = serializers.SerializerMethodField()
 
@@ -213,9 +210,6 @@ class CurrentUserSerializer(ChannelRelationshipSerializer):
             'slug',
             'public',
             'organizations',
-            'rating_requests_user_count',
-            'rating_requests_project_count',
-            'rating_requests_projects_with_unrated_users',
             'is_subscribed_to_newsletter',
             'chat_enabled',
             'document',
@@ -253,28 +247,6 @@ class CurrentUserSerializer(ChannelRelationshipSerializer):
             user=obj
         )
         return applies.count() > 0
-
-    def get_rating_requests_user_count(self, obj):
-        return obj.rating_requests.filter(
-            deleted_date=None,
-            rating=None,
-            content_type__model="user").count()
-
-    def get_rating_requests_project_count(self, obj):
-        return obj.rating_requests.filter(
-            deleted_date=None,
-            rating=None,
-            content_type__model="project").count()
-
-    def get_rating_requests_projects_with_unrated_users(self, obj):
-        return len(
-            obj.rating_requests.filter(
-                deleted_date=None,
-                rating=None,
-                content_type__model="user",
-                initiator_type__model="project").values_list(
-                "initiator_id",
-                flat=True).distinct())
 
     @expired_password
     def to_representation(self, *args, **kwargs):
